da34187fcf24dd06e13f22b81ec3df15be98c09c
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-09-14 14:57:40 -0400

    display blogs on page

--- index.js
+++ index.js
@@ -13,9 +13,25 @@
  Log the 5 items to the console.
  */
 
+/**
+ Challenge:
+
+ With the 5 blog post objects, display the `title` and `body`
+properties of the first 5 posts on the browser page.
+ 
+ Hints: 
+ * Create a `div` in the HTML file to store these items
+ * Loop over the items creating a string of HTML elements you 
+   can then put into the div with `innerHTML`
+ */
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((data) => {
     const postsArr = data.slice(0, 5);
     console.log(postsArr);
+    for (let post of postsArr) {
+      document.getElementById("posts").innerHTML += post.title;
+      document.getElementById("posts").innerHTML += post.body;
+    }
   });
