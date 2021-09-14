508d6c511fc2a8d66f87b1fdcbdbaec6ced68ac1
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-09-14 15:02:43 -0400

    refactor (remove innerHTML from loop)

--- index.js
+++ index.js
@@ -29,9 +29,13 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((data) => {
     const postsArr = data.slice(0, 5);
-    console.log(postsArr);
+
+    let blogPosts = "";
     for (let post of postsArr) {
-      document.getElementById("posts").innerHTML += post.title;
-      document.getElementById("posts").innerHTML += post.body;
+      blogPosts += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        `;
     }
+    document.getElementById("posts").innerHTML = blogPosts;
   });
