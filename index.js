a6fb25ee24b573e377cfffd6c6fa65fe82cbca5d
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-11-29 11:43:14 -0500

    adding new post (not correct)

--- index.js
+++ index.js
@@ -14,15 +14,6 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     document.getElementById("posts").innerHTML = blogPosts;
   });
 
-/**
- Challenge:
- 
- * Listen for the "submit" event on the form (which will happen when the button is clicked)
-    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
- * Combine the title value and body value into an object (with a "title" property and "body" property)
- * Log the object to the console
-
-*/
 document.getElementById("new-post").addEventListener("submit", function (e) {
   e.preventDefault();
   const postTitle = document.getElementById("post-title").value;
@@ -32,15 +23,6 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
     title: postTitle,
     body: postBody,
   };
-  /**
-   * Challenge: Send this off to the server!
-   *
-   * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
-   * 2. Endpoint: /posts
-   * 3. method: ???
-   * 4. Request body: ??? (Remember to turn it into JSON)
-   * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
-   */
   fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
     method: "POST",
     headers: {
@@ -50,6 +32,9 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
   })
     .then((res) => res.json())
     .then((data) => console.log(data));
+  document.getElementById("posts").before = `<h3>${title}</h3>
+  <p>${body}</p>
+  <hr/>`;
 });
 
 /**
