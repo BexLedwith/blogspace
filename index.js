a6e9f6fd8b0624e9da390da2fd18c1cd162054f4
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-11-29 11:21:46 -0500

    request body practice

--- index.js
+++ index.js
@@ -34,3 +34,30 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
   };
   console.log(data);
 });
+
+/**
+Challenge:
+
+- Send a request to add a new todo item
+    - BaseURL: https://apis.scrimba.com/jsonplaceholder/
+    - Endpoint: /todos
+- Body object should contain a "title" property (string) 
+  and a "completed" property (boolean)
+- Don't add the .then() blocks yet - we have 1 more thing 
+  to do before it'll work correctly
+
+  Part 2 - add the .then blocks to check the data coming back
+
+*/
+
+fetch("https://apis.scrimba.com/jsonplaceholder/todos", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "Buy Milk",
+    completed: false,
+  }),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
