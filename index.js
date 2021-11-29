acdb4a88b4a7667f99c3af5bbd3126e53b6f3354
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-11-29 11:11:22 -0500

    form submit event listener

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-/**
- Challenge:
- 
- GET a list of blog posts from the JSON Placeholder API.
- 
- BaseURL: https://apis.scrimba.com/jsonplaceholder/
- Endpoint: /posts
- 
- Since there's so many posts, let's limit the array to just 5 items.
- You can use the `.slice()` array method to just grab the first 5 objects
- from the data array that comes back from the API
-
- Log the 5 items to the console.
- */
-
-/**
- Challenge:
-
- With the 5 blog post objects, display the `title` and `body`
-properties of the first 5 posts on the browser page.
- 
- Hints: 
- * Create a `div` in the HTML file to store these items
- * Loop over the items creating a string of HTML elements you 
-   can then put into the div with `innerHTML`
- */
-
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((data) => {
@@ -40,3 +13,24 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     }
     document.getElementById("posts").innerHTML = blogPosts;
   });
+
+/**
+ Challenge:
+ 
+ * Listen for the "submit" event on the form (which will happen when the button is clicked)
+    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
+ * Combine the title value and body value into an object (with a "title" property and "body" property)
+ * Log the object to the console
+
+*/
+document.getElementById("new-post").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const postTitle = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body").value;
+
+  const data = {
+    title: postTitle,
+    body: postBody,
+  };
+  console.log(data);
+});
